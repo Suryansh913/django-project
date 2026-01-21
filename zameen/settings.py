ab9dd240b41bc0c6a70eb4c7578ddc201cf55c99
Your Name <your-email@example.com>
2026-01-21 18:18:53 +0530

    Fix settings for production: DEBUG=False and configure STATIC_ROOT

--- zameen/settings.py
+++ zameen/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3a@=a=ir!)nxbf3ru342_6(3o@b0m4!8$v5dyuy*c9!4=fy_%0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ 
     'django-project-2-3l3e.onrender.com',
@@ -126,10 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-    # You can add other directories here if needed, e.g.:
-    # '/var/www/myproject_static',
 ]
 
